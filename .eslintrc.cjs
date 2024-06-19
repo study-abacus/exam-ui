2a99f84989a05eabfaf1f7da461b8c45a9481938
Jatin Katyal <jatin.katyal13@gmail.com>
2024-06-19 21:55:24 +0530

    Added: CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react-hooks/rules-of-hooks': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   },
 }
 
