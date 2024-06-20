bf5c113ae2829696bf6f9a366379a1acfd1ad089
Prerna Singh <writeprernasingh@gmail.com>
2024-06-20 10:52:07 +0530

    Create profile.ts

---
+++ src/api/endpoints/profile.ts
@@ -0,0 +1,31 @@
+import { useQuery, useMutation } from 'react-query'
+import { client } from '~/api/client'
+
+export const getProfile = (profileId: number) =>
+    useQuery(["profile", profileId], async () => {
+        const response = await client.get(`api/v1/profile/${profileId}/`);
+        return response.data;
+    }, {
+        enabled: profileId
+    })
+
+export type UpdateProfileParams = {
+    data : {
+        name : string,
+        ci : string,
+        phone : string,
+        email : string,
+        guardian_name : string
+    }
+
+}
+export const updateProfile = () =>
+    useMutation({
+        mutationFn: async ({ data }: UpdateProfileParams) => {
+            const response = await client.put(
+                `api/v1/profile/`,
+                data
+            );
+            return response.data;
+        }
+    })
